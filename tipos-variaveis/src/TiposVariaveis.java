a515d0cf1baefd6522aeceb447c609608720feca
davidcsoares <davidcamilosoares@outlook.com>
2022-10-03 15:48:04 -0300

    criando projeto sobre operadores em java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //Os tipos primitivos são int, byte, short, long, float e double
+        //Sendo int, byte, short
+        //Exemplos de da estrutura da variavel 
+        int idade;
+        int anoDeEstudo = 2022;
+        double salarioMinimo = 2500.00;
+        byte quantidadeSalas = 70;
+        int quantidadeEpisodiosTwd = 177;
+
+        //coordenadas de lisboa - portugal
+        double latitude = 38.7071;
+        double longitute =  -9.13549;
+
+        //quantidade de brasileiros em 2022
+        double quantidadePopulacaoBrasil = 215000000.00;
+
+
+
+        System.out.println(quantidadeEpisodiosTwd);
+        System.out.println(quantidadePopulacaoBrasil);
+        System.out.println(latitude);
+        System.out.println(longitute);
+        
+
     }
 }
