415cdaf0c944f2c14cb5d508d773a7feede5573a
davidcsoares <davidcamilosoares@outlook.com>
2022-10-05 17:52:07 -0300

    criando classe smarttv

---
+++ metodos-java/sistema-smart-tv/src/SmartTv.java
@@ -0,0 +1,36 @@
+public class SmartTv {
+
+    //atributos da classe SmartTv
+    boolean ligada = false;
+    int canal = 12;
+    int volume = 34;
+
+    //metodo ligar
+    public void ligar(){
+        ligada = true;
+    }
+
+    public void desligar(){
+        ligada = false;
+    }
+
+    public void escolherCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void mudarCanal() {
+        canal++;
+    }
+
+    public void voltarCanal() {
+        canal--;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+}
