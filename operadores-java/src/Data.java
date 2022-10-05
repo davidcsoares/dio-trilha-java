3a733057ef7616692a6e5bee48f75a5d98566d8d
davidcsoares <davidcamilosoares@outlook.com>
2022-10-05 10:21:57 -0300

    projeto sobre operadores em java

---
+++ operadores-java/src/Data.java
@@ -0,0 +1,8 @@
+public class Data {
+
+    //exemplo de classe
+    String numeroDia;
+    String mes;
+    int ano;
+}
+
--- operadores-java/src/Operadores.java
+++ operadores-java/src/Operadores.java
@@ -1,5 +1,62 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        /*op de atribuição (=)
+        String uf = "CE";
+
+        //op aritimetico (+, -, * e /)
+        int soma = 5 + 5;
+
+        //classe Operadores.java
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        System.out.println(nomeCompleto);
+		
+        //qual o resultado das expressoes abaixo?
+        String concatenacao ="?"; 
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);*/
+
+        //classe Operadores.java
+        int numero = 5;
+                
+        //Imprimindo o numero negativo
+        System.out.println(- numero);
+
+        //incrementando numero em mais 1 numero, imprime 6
+        numero ++;
+        System.out.println(numero);
+
+        //incrementando numero em mais 1 numero, imprime 7
+        System.out.println(numero ++);// ops algo de errado não está certo
+
+        System.out.println(numero);// agora sim, numero virou 7
+
+        //ordem de precedencia conta aqui
+        System.out.println(++ numero);
+
+        boolean verdadeiro = true;
+
+        System.out.println("Inverteu " + !verdadeiro);
     }
 }
